899403f7e59077ba8b8c05cf4d2b133c35046a61
gasous30 <youngblue.black@gmail.com>
2022-01-11 21:33:13 +0700

    fix: less memory use

--- motion_detector.py
+++ motion_detector.py
@@ -43,6 +43,7 @@ while True:
         cv2.rectangle(frame, (x,y), (x+w, y+h), (0, 255, 0), 3)
 
     status_list.append(status)
+    status_list = status_list[-2:]
     if not status_list[-1] == status_list[-2]:
         times.append(datetime.now())
 
